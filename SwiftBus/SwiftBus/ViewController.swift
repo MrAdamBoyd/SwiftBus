c7a498d32ec8afb92cbc9d499c85a4b42caf29d1
Adam Boyd <adamboyd50@gmail.com>
2015-08-29 18:55:22 -0400

    initial xcode project

---
+++ SwiftBus/SwiftBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBus
+//
+//  Created by Adam on 2015-08-29.
+//  Copyright (c) 2015 com.adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
