4a775fa1518f130a1a1407b7423ec6f3c1f36f35
Adam Boyd <adamboyd50@gmail.com>
2015-09-01 23:18:42 -0400

    example up and running

--- SwiftBus/SwiftBus/ViewController.swift
+++ SwiftBus/SwiftBus/ViewController.swift
@@ -13,43 +13,68 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+    }
+    
+    
+    @IBAction func agencyListTouched(sender: AnyObject) {
         SwiftBus.sharedController.transitAgencies({(agencies:[String : TransitAgency]) -> Void in
-            println(agencies.count)
-            println(agencies["sf-muni"])
+            println("Number of agencies loaded: \(agencies.count)")
+            for agency in agencies.values {
+                println("Name: " + agency.agencyTitle)
+            }
+            println()
         })
-        
+    }
+
+    
+    @IBAction func routesForAgencyTouched(sender: AnyObject) {
         SwiftBus.sharedController.routesForAgency("sf-muni", closure: {(agencyRoutes:[String : TransitRoute]) -> Void in
-            println(agencyRoutes.count)
+            println("Number of routes loaded for SF MUNI: \(agencyRoutes.count)")
+            for route in agencyRoutes.values {
+                println("Route title: " + route.routeTitle)
+            }
+            println()
         })
         
-        SwiftBus.sharedController.routesForAgency("sf-muni", closure: {(agencyRoutes:[String : TransitRoute]) -> Void in
-            println(agencyRoutes.count)
-        })
-        
+    }
+    
+    @IBAction func routeConfigurationTouched(sender: AnyObject) {
         SwiftBus.sharedController.routeConfiguration("5R", forAgency: "sf-muni", closure: {(route:TransitRoute?) -> Void in
-            
             //If the route exists
             if let transitRoute = route as TransitRoute! {
-                println(transitRoute.routeColor)
+                println("Route config for route " + transitRoute.routeTitle)
+                println("Number of stops on route in one direction: \(transitRoute.stopsOnRoute.values.array[0].count)")
+                println()
             }
             
         })
+        
+    }
+    
+    @IBAction func vehicleLocationsTouched(sender: AnyObject) {
         
         SwiftBus.sharedController.vehicleLocationsForRoute("N", forAgency: "sf-muni", closure:{(route:TransitRoute?) -> Void in
             if let transitRoute = route as TransitRoute! {
-                println(transitRoute.vehiclesOnRoute.count)
+                println("\(transitRoute.vehiclesOnRoute.count) vehicles on route N Judah")
+                println("Example vehicle:Vehcle ID: \(transitRoute.vehiclesOnRoute[0].vehicleId), \(transitRoute.vehiclesOnRoute[0].speedKmH) Km/h, \(transitRoute.vehiclesOnRoute[0].lat), \(transitRoute.vehiclesOnRoute[0].lon), seconds since report: \(transitRoute.vehiclesOnRoute[0].secondsSinceReport)")
+                println()
             }
         })
         
+    }
+    
+    @IBAction func stopPredictionsTouched(sender: AnyObject) {
         SwiftBus.sharedController.stopPredictions("3909", onRoute: "N", withAgency: "sf-muni", closure: {(route:TransitStop?) -> Void in
             
             //If the stop and route exists
             if let transitStop = route as TransitStop! {
-                println(transitStop.combinedPredictions())
+                println("Stop: \(transitStop.stopTitle)")
+                println("Predictions at stop \(transitStop.combinedPredictions()) mins")
+                println()
             }
             
         })
+        
     }
 
     override func didReceiveMemoryWarning() {
