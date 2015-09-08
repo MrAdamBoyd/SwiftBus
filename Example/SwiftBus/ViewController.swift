dc1072b1ff1b6998b51a71f3af846b4992d45444
Adam Boyd <adamboyd50@gmail.com>
2015-09-07 21:53:14 -0400

    using pod lib create

---
+++ Example/SwiftBus/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftBus
+//
+//  Created by Adam Boyd on 09/07/2015.
+//  Copyright (c) 2015 Adam Boyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
